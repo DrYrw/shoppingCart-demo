64502b01b74ef73df04cc59348b1981aaa135d07
DrYrw <52146550@qq.com>
2015-11-30 16:13:23 +0800

    Initial Commit

---
+++ YY购物demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YY购物demo
+//
+//  Created by 杨玥 on 15/11/30.
+//  Copyright © 2015年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YY购物demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YY购物demo
+//
+//  Created by 杨玥 on 15/11/30.
+//  Copyright © 2015年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
